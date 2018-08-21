04f97ca77946881658dd98afe1cd347be420ac88
D'Arcy Matthews-Nally <u5561865@anu.edu.au>
2018-08-21 17:06:05 +1000

    Corrected parsing of quaternion data. Added conversion to axis angles.

--- DataAnalysis/IMU_log_grapher.py
+++ DataAnalysis/IMU_log_grapher.py
@@ -35,7 +35,7 @@ def quaternion_to_euler_angle(w, x, y, z):
     # put quat x y z w into quadruplet into list
     # put temp into list
 def main(argv):
-    file = open(argv[1])
+    file = open(argv[0])
     reader = csv.reader(file, delimiter=',')
 
     total_lines = 0
@@ -72,9 +72,9 @@ def main(argv):
                     acc.append((float(acc_temp[0]),\
                                 float(acc_temp[1]),\
                                 float(col)))
-                if col_i == 7 or col_i == 8 or col_i == 9: # quat w, x, y
+                if col_i == 7 or col_i == 8 or col_i == 9: # quat x, y, z
                     quat_temp.append(col)
-                if col_i == 10: # quat z
+                if col_i == 10: # quat w
                     quat.append((float(quat_temp[0]),\
                                  float(quat_temp[1]),\
                                  float(quat_temp[2]),\
@@ -103,10 +103,12 @@ def main(argv):
         if index == 0:
             time_since_previous.append(0)
         else:   
-            time_since_previous.append((datetime.datetime.strptime(tstamp[index],\
+            time_since_previous.append((datetime.datetime.strptime(\
+                                                            tstamp[index],\
                                                             time_fmt)) -\
-                                  datetime.datetime.strptime(tstamp[index-1],\
-                                                       time_fmt))
+                                        datetime.datetime.strptime(\
+                                                            tstamp[index-1],\
+                                                            time_fmt))
             average_data_rate += (time_since_previous[index].microseconds)
 
     average_data_rate = average_data_rate / total_lines
@@ -115,62 +117,95 @@ def main(argv):
     average_data_rate_string = "Average IMU read rate: " +\
                                str(round(average_data_rate, 3)) +\
                                " (Hz)"
-    if argv[0] == 'q':
-        # generate plotting data
-        w_data = []
-        x_data = []
-        y_data = []
-        z_data = []
-        for index in i:
-            w_data.append(quat[index][0])
-            x_data.append(quat[index][1])
-            y_data.append(quat[index][2])
-            z_data.append(quat[index][3])
 
-        # draw all quaternions 
-        plt.subplot(411)
-        plt.title("Orientation in quaternions\n"+average_data_rate_string)
-        plt.plot(elapsed_time, w_data)
-        plt.ylabel("w")
-        plt.subplot(412)
-        plt.plot(elapsed_time, x_data)
-        plt.ylabel("x")
-        plt.subplot(413)
-        plt.plot(elapsed_time, y_data)
-        plt.ylabel("y")
-        plt.subplot(414)
-        plt.plot(elapsed_time, z_data)
-        plt.ylabel("z")
-        plt.xlabel("elapsed seconds") 
+    x_data = []
+    y_data = []
+    z_data = []
+    w_data = []
+    for index in i:
+        x_data.append(quat[index][0])
+        y_data.append(quat[index][1])
+        z_data.append(quat[index][2])
+        w_data.append(quat[index][3])
+    # convert all data to euler angles
+    eul_x = []
+    eul_y = []
+    eul_z = []
+    for index in i: 
+        eul_temp = quaternion_to_euler_angle(quat[index][0],\
+                                             quat[index][1],\
+                                             quat[index][2],\
+                                             quat[index][3])
+        eul_x.append(eul_temp[0])
+        eul_y.append(eul_temp[1])
+        eul_z.append(eul_temp[2])
 
-    elif argv[0] == 'e':
-        # convert all data to euler angles
-        eul_x = []
-        eul_y = []
-        eul_z = []
-        for index in i: 
-            eul_temp = quaternion_to_euler_angle(quat[index][0],\
-                                                 quat[index][1],\
-                                                 quat[index][2],\
-                                                 quat[index][3])
-            eul_x.append(eul_temp[0])
-            eul_y.append(eul_temp[1])
-            eul_z.append(eul_temp[2])
-        
-        # draw all euler angles     
-        plt.subplot(311)
-        plt.title("Orientation in Euler angles\n"+average_data_rate_string)
-        plt.plot(elapsed_time, eul_x)
-        plt.ylabel("roll (degrees)")
-        plt.subplot(312)
-        plt.plot(elapsed_time, eul_y)
-        plt.ylabel("pitch (degrees)")
-        plt.subplot(313)
-        plt.plot(elapsed_time, eul_z)
-        plt.ylabel("yaw (degrees)")
-        plt.xlabel("elapsed seconds (s)")
+    # convert quaternions to axis angles (position relative to origin)
+    ax_angle = []
+    ax_x = []
+    ax_y = []
+    ax_z = []
+    #http://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToAngle/index.htm
+        # angle = 2 * acos(qw)
+        # x = qx / sqrt(1-qw*qw)
+        # y = qy / sqrt(1-qw*qw)
+        # z = qz / sqrt(1-qw*qw)
+    eq_part = math.sqrt(1 - (w_data[index] * w_data[index]))
+    for index in i:
+        ax_angle.append(2 * math.acos(w_data[index]))
+        ax_x.append(x_data[index] / eq_part)
+        ax_y.append(y_data[index] / eq_part)
+        ax_z.append(z_data[index] / eq_part)
 
-    
+
+    # draw all quaternions 
+    plt.figure(1)
+    plt.subplot(411)
+    plt.title("Orientation in quaternions\n"+average_data_rate_string)
+    plt.plot(elapsed_time, w_data)
+    plt.ylabel("w")
+    plt.subplot(412)
+    plt.plot(elapsed_time, x_data)
+    plt.ylabel("x")
+    plt.subplot(413)
+    plt.plot(elapsed_time, y_data)
+    plt.ylabel("y")
+    plt.subplot(414)
+    plt.plot(elapsed_time, z_data)
+    plt.ylabel("z")
+    plt.xlabel("elapsed seconds") 
+
+    # draw all euler angles
+    plt.figure(2)  
+    plt.subplot(311)
+    plt.title("Orientation in Euler angles\n"+average_data_rate_string)
+    plt.plot(elapsed_time, eul_x)
+    plt.ylabel("roll (degrees)")
+    plt.subplot(312)
+    plt.plot(elapsed_time, eul_y)
+    plt.ylabel("pitch (degrees)")
+    plt.subplot(313)
+    plt.plot(elapsed_time, eul_z)
+    plt.ylabel("yaw (degrees)")
+    plt.xlabel("elapsed seconds (s)")
+
+    # draw all axis angles 
+    plt.figure(3)
+    plt.subplot(221)
+    plt.title("Orientation in axis angles\n"+average_data_rate_string)
+    plt.plot(elapsed_time, ax_angle)
+    plt.ylabel("angle")
+    plt.subplot(222)
+    plt.plot(elapsed_time, ax_x)
+    plt.ylabel("x")
+    plt.subplot(223)
+    plt.plot(elapsed_time, ax_y)
+    plt.ylabel("y")
+    plt.subplot(224)
+    plt.plot(elapsed_time, ax_z)
+    plt.ylabel("z")
+    plt.xlabel("elapsed seconds") 
+
     plt.show()
 
 if __name__ == "__main__":
